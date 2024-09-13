df2b6796efca8f343d178dc0e2ae11a48f22611b
Annaswa <anaswasetia@gmail.com>
2024-09-13 09:07:02 +0700

    membuat managed rooting, migration, dan model

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,16 @@ Auth::routes([
     'verify' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//middleware untuk membatasi user
+Route::group([
+    'middleware' => ['auth'],
+    'prefix' => 'admin', //admin/tamu
+    'as' => 'admin.', // routea(admin.)
+], function () {
+
+    //guestbook.test/admin/
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index'); 
+    
+    //guestbook.test/admin/dashboard -> route(admin.dashboard)
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+});
